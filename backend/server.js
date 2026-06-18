b175b3e72e420c88f3de8b2dc741da209be60d11
Vishal <vishalvishwkarma553@gmail.com>
2026-06-18 23:13:51 +0530

    added frontend url in cors

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://review-booking.vercel.app"],
   credentials: true,
 }));
 app.use(express.json());
